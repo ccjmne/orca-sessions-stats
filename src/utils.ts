a85c5c4672585db997ad5eae15fcde333b142b35
ccjmne <ccjmne@gmail.com>
2019-12-10 19:11:56 +0100

    Tweak transitions signatures

--- src/utils.ts
+++ src/utils.ts
@@ -4,10 +4,14 @@ export function endOfMonth(date: Date): Date {
   return new Date(Date.UTC(date.getFullYear(), date.getMonth() + 1, 0));
 }
 
-export function snapTransition<Datum>(e: Selection<SVGElement, Datum, any, any>, easing: (normalizedTime: number) => number = easeExpOut) {
+export function snapTransition<E extends SVGElement, Datum>(
+  e: Selection<E, Datum, any, any>,
+  easing: (normalizedTime: number) => number = easeExpOut) {
   return e.transition().duration(100).ease(easing);
 }
 
-export function slowTransition<Datum>(e: Selection<SVGElement, Datum, any, any>, easing: (normalizedTime: number) => number = easeExpOut) {
+export function slowTransition<E extends SVGElement, Datum>(
+  e: Selection<E, Datum, any, any>,
+  easing: (normalizedTime: number) => number = easeExpOut) {
   return e.transition().duration(500).ease(easing);
 }
