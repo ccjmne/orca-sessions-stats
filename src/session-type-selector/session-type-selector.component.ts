2f0a0b2a97ef57b082bec82f1ad56cb8b98dabc8
ccjmne <ccjmne@gmail.com>
2019-12-27 15:37:45 +0100

    Fire session-type-selector hook on init

--- src/session-type-selector/session-type-selector.component.ts
+++ src/session-type-selector/session-type-selector.component.ts
@@ -2,18 +2,28 @@ import { IComponentOptions } from 'angular';
 import SESSION_TYPES from 'assets/session-types.json';
 
 export const sessionTypeSelectorComponent: IComponentOptions = {
-  template: `<select ng-options="opt.trty_pk as opt.trty_name for opt in $ctrl.types" ng-model="$ctrl.type" ng-change="$ctrl.typeChanged({ type: $ctrl.type })"></select>`,
+  template: `<select ng-options="opt.trty_pk as opt.trty_name for opt in $ctrl.types" ng-model="$ctrl.type"></select>`,
   bindings: {
     typeChanged: '&?'
   },
   controller: class SessionTypeSelectorController {
 
     // angular bindings
-    public typeChanged: (type: number) => any;
+    public typeChanged: (context: { type: number }) => any;
 
     // template bindings
     protected types: any[] = Object.values(SESSION_TYPES);
-    protected type: any;
+
+    private _type: number;
+    protected get type(): number {
+      return this._type;
+    }
+    protected set type(type: number) {
+      this._type = type;
+      if (this.typeChanged) {
+        this.typeChanged({ type });
+      }
+    }
 
     public $onInit(): void {
       this.type = this.types[0].trty_pk;
