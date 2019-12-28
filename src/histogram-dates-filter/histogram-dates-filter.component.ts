5f7fc6ec059b1fc7d6991e27af73b74e8ceb1a1b
ccjmne <ccjmne@gmail.com>
2019-12-28 22:51:04 +0100

    Use newer stacked-barchart in histogram-dates cmpt

--- src/histogram-dates-filter/histogram-dates-filter.component.ts
+++ src/histogram-dates-filter/histogram-dates-filter.component.ts
@@ -1,31 +1,33 @@
-import { IComponentOptions, IWindowService, IAugmentedJQuery, IScope } from 'angular';
+import { IComponentOptions, IWindowService, IAugmentedJQuery, IScope, IOnChangesObject } from 'angular';
 import { Discriminator, PopulationCode } from 'src/population.class';
 import { Dimension, Group } from 'crossfilter2';
 import { SessionRecord } from 'src/record.class';
-import { StackedBarsComponent } from 'src/stacked-bars/stacked-bars.component';
 import { Stack, stack, stackOrderNone, stackOffsetNone, hsl } from 'd3';
 import { Outcome } from 'src/outcome.class';
 import { REFRESH_EVENT } from 'src/refresh-event.class';
+import { takeUntil } from 'rxjs/operators';
+import { componentDestroyed } from 'src/component-destroyed';
+import { StackedBarchartComponent } from 'src/stacked-barchart/stacked-barchart.component';
 
-type Entry = { from: Date, to: Date } & Partial<Record<PopulationCode, number>>;
+type Entry = { date: Date } & Partial<Record<PopulationCode, number>>;
 
 export const histogramDatesFilterComponent: IComponentOptions = {
   template: require('./histogram-dates-filter.component.html'),
   bindings: {
     discriminator: '<',
     outcome: '<',
-    dates: '<'
+    dates: '<',
+    onSelect: '&'
   },
-  controller: class HistogramDatesFilterController extends StackedBarsComponent<Entry> {
+  controller: class HistogramDatesFilterController extends StackedBarchartComponent<Entry> {
+
     public $inject: string[] = ['$scope', '$element', '$window'];
 
     // angular bindings
     public discriminator: Discriminator;
     public outcome: Outcome;
     public dates: Dimension<SessionRecord, Date>;
-
-    // template bindings
-    protected group: Group<SessionRecord, Date, Partial<Record<PopulationCode, number>>>;
+    public onSelect: (e: { selected: { date: Date } | null }) => any;
 
     // abstract overrides
     protected stacker: Stack<any, Entry, string>;
@@ -35,44 +37,52 @@ export const histogramDatesFilterComponent: IComponentOptions = {
       return String(hsl(h, s * (i ? .75 : 1), l * (i ? .75 : 1)));
     }
 
+    // data
+    private group: Group<SessionRecord, Date, Partial<Record<PopulationCode, number>>>;
+
     constructor($scope: IScope, $element: IAugmentedJQuery, $window: IWindowService) {
       super($element, $window);
-      $scope.$on(REFRESH_EVENT, () => this.$onChanges());
+      $scope.$on(REFRESH_EVENT, () => this.refresh());
     }
 
     public $onInit(): void {
       super.$onInit();
+
+      this.selected$.pipe(
+        takeUntil(componentDestroyed(this))
+      ).subscribe(selected => this.onSelect && this.onSelect({ selected }));
     }
 
-    public $onChanges(): void {
+    public $onChanges(changes: IOnChangesObject): void {
       if (this.dates && this.discriminator) {
-        if (this.group) {
-          this.group.dispose();
+        if (changes['dates'] || changes['discriminator']) {
+          if (this.group) {
+            this.group.dispose();
+          }
+
+          this.group = this.dates.group<Date, Partial<Record<PopulationCode, number>>>().reduce(
+            (acc, d) => (this.discriminator.populations.forEach(({ id, is }) => is(d) && acc[id]++), acc),
+            (acc, d) => (this.discriminator.populations.forEach(({ id, is }) => is(d) && acc[id]--), acc),
+            () => this.discriminator.populations.map(({ id }) => id).reduce((acc, pop) => ({ ...acc, [pop]: 0 }), {})
+          );
+
+          this.stacker = stack<Entry, PopulationCode>()
+            .keys(this.discriminator.populations.map(({ id }) => id))
+            .value((values, key) => values[key])
+            .order(stackOrderNone)
+            .offset(stackOffsetNone);
         }
-
-        this.group = this.dates.group<Date, Partial<Record<PopulationCode, number>>>().reduce(
-          (acc, d) => (this.discriminator.populations.forEach(({ id, is }) => is(d) && acc[id]++), acc),
-          (acc, d) => (this.discriminator.populations.forEach(({ id, is }) => is(d) && acc[id]--), acc),
-          () => this.discriminator.populations.map(({ id }) => id).reduce((acc, pop) => ({ ...acc, [pop]: 0 }), {})
-        );
-
-        this.stacker = stack<Entry, PopulationCode>()
-          .keys(this.discriminator.populations.map(({ id }) => id))
-          .value((values, key) => values[key])
-          .order(stackOrderNone)
-          .offset(stackOffsetNone);
 
         this.refresh();
       }
     }
 
     public refresh(): void {
-      this.data = this.group.all().map(({ key: from, value: values }) => ({
-        ...values,
-        from: new Date(from),
-        to: new Date(new Date(from).getFullYear(), new Date(from).getMonth() + 1, 0)
-      }));
+      if (!this.group) {
+        return;
+      }
 
+      this.data = this.group.all().map(({ key: date, value: values }) => ({ ...values, date: new Date(date) }));
       super.refresh();
     }
 
