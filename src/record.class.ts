efa3e1c0cd89546e642e2f187ae7b3e12336f376
ccjmne <ccjmne@gmail.com>
2019-12-31 22:41:26 +0100

    Add trng_outcome attribute to session-records

--- src/record.class.ts
+++ src/record.class.ts
@@ -1,9 +1,11 @@
 import { OutcomeCode } from './outcome.class';
 
 export type Month = Date;
+export type SessionOutcome = 'COMPLETED' | 'CANCELLED' | 'PENDING';
 
 export interface SessionRecord {
-  trem_pk: number;
+  trng_pk: number;
+  trng_outcome: SessionOutcome;
   instructors: number[];
   empl_gender: true | false;
   trem_outcome: OutcomeCode;
