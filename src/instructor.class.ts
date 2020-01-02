0bf9504196bc90049e3261cd4ff076356ad7ffbe
ccjmne <ccjmne@gmail.com>
2020-01-02 19:43:46 +0100

    Fetch Instructor data

---
+++ src/instructor.class.ts
@@ -0,0 +1,11 @@
+export interface Instructor {
+  empl_pk: number;
+  empl_firstname: string;
+  empl_surname: string;
+  empl_dob: Date;
+  empl_permanent: boolean;
+  empl_address: string;
+  empl_notes: string;
+  empl_gender: boolean;
+  empl_external_id: string;
+}
